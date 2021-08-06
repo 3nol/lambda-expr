8fe7b619ce36f4bc6e95cd3bec512e17b8d8a022
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2021-08-06 10:37:32 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lambda_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Lambda Expressions Parser
+
+meh
